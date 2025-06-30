f4f053d75a448fcec4f4b236cce4381daee8e0d5
Alexis <alecraft291@gmail.com>
2025-06-30 17:41:04 -0500

    feat se crea la pagina de carrito y se le añaden estilos (me puse a chimbear con el js tambien y otras cositas), creo que ya culmine

--- Alexis/js.js
+++ Alexis/js.js
@@ -1,8 +1,6 @@
-// Selección de elementos
 const btn = document.getElementById("modoOscuroBtn");
 const body = document.body;
 
-// Cargar preferencia guardada
 if (localStorage.getItem("modo") === "oscuro") {
 body.classList.add("dark-mode");
 btn.classList.remove("btn-outline-light");
@@ -10,7 +8,6 @@ btn.classList.add("btn-outline-warning");
 btn.innerHTML = '<i class="bi bi-sun-fill"></i>';
 }
 
-// Evento para alternar modo
 btn.addEventListener("click", () => {
 body.classList.toggle("dark-mode");
 const esOscuro = body.classList.contains("dark-mode");
@@ -22,4 +19,32 @@ btn.innerHTML = esOscuro
     : '<i class="bi bi-moon-fill"></i>';
 
 localStorage.setItem("modo", esOscuro ? "oscuro" : "claro");
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cantidadInputs = document.querySelectorAll(".cantidad-input");
+    const subtotalSpan = document.getElementById("subtotal");
+    const totalSpan = document.getElementById("total");
+    const envio = 15000;
+    function formatearNumero(numero) {
+    return numero.toLocaleString("es-CO");
+}
+    function actualizarResumen() {
+    let subtotal = 0;
+    cantidadInputs.forEach((input) => {
+        const precio = parseFloat(input.dataset.precio);
+        const cantidad = parseInt(input.value);
+        subtotal += precio * cantidad;
+});
+const total = subtotal + envio;
+    subtotalSpan.textContent = formatearNumero(subtotal);
+    totalSpan.textContent = formatearNumero(total);
+}
+    cantidadInputs.forEach((input) => {
+    input.addEventListener("input", actualizarResumen);
+});
+
+    actualizarResumen();
 });
